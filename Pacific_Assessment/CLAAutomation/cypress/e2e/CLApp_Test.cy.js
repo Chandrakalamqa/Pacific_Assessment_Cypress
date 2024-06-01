f3a0501febc800837f57ce3a7070b01e9e41cae7
Chandrakalamqa <chandrakalamqa@gmail.com>
2024-06-01 08:18:42 +0530

    Update CLApp_Test.cy.js

--- Pacific_Assessment/CLAAutomation/cypress/e2e/CLApp_Test.cy.js
+++ Pacific_Assessment/CLAAutomation/cypress/e2e/CLApp_Test.cy.js
@@ -4,22 +4,22 @@ const filePath = serverUrl+'/index.html';
 
 describe('Verify page title and header', () => {
     it('should have the correct page title', () => {
-        cy.visit(filePath); // Replace with your actual file path or URL
+        cy.visit(filePath);
         cy.title().should('eq', 'Sample Page');
     });
 
     it('should display the correct header', () => {
-        cy.visit(filePath); // Replace with your actual file path or URL
+        cy.visit(filePath);
         cy.get('h1').should('be.visible').and('have.text', 'Welcome to the Sample Page');
     });
 
     it('should have a login button with the text "Login"', () => {
-        cy.visit(filePath); // Replace with your actual file path or URL
+        cy.visit(filePath);
         cy.get('#login-btn').should('be.visible').and('have.text', 'Login');
     });
 
     it('should keep the hidden section hidden after invalid login attempt', () => {
-        cy.visit(filePath); // Replace with your actual file path or URL
+        cy.visit(filePath);
         cy.get('#username').should('be.visible').type('invalidUser');
         cy.get('#password').should('be.visible').type('invalidPass');
         cy.get('#login-btn').should('be.visible').click();
@@ -35,7 +35,7 @@ describe('Verify page title and header', () => {
     });
 
     it('should have the correct class and styling for the login button', () => {
-        cy.visit(filePath); // Replace with your actual file path or URL
+        cy.visit(filePath);
         cy.get('#login-btn')
             .should('be.visible')
             .and('have.class', 'btn')
@@ -47,7 +47,7 @@ describe('Verify page title and header', () => {
     });
 
     it('should have the correct headers and data in the information section', () => {
-        cy.visit(filePath); // Replace with your actual file path or URL
+        cy.visit(filePath);
 
         cy.get('.header-section p').should('have.length', 3);
         cy.get('.header-section p').eq(0).should('have.text', 'Country');
